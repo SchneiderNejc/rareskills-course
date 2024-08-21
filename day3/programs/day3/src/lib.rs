f3082bc7a2e403a2d7e0bcad3025dc77aded57ab
Nejc Schneider <nejc.sch@gmail.com>
2024-08-21 19:47:26 +0200

    change struct name to BoatyMcBoatface

--- day3/programs/day3/src/lib.rs
+++ day3/programs/day3/src/lib.rs
@@ -6,35 +6,35 @@ declare_id!("J4JFzPXckHctGsYtDR1gPueCoaXycNCJ3tei533XPB2");
 pub mod day3 {
     use super::*;
 
-    pub fn boaty_mc_boatface(ctx: Context<Initialize>) -> Result<()> {
+    pub fn boaty_mc_boatface(ctx: Context<BoatyMcBoatface >) -> Result<()> {
         Ok(())
     }
 
-    pub fn add(ctx: Context<Initialize>, a: u64, b: u64) -> Result<()> {
+    pub fn add(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
         let result: u64 = a + b;
         msg!("Result of addition: {}", result);
         Ok(())
     }
 
-    pub fn sub(ctx: Context<Initialize>, a: u64, b: u64) -> Result<()> {
+    pub fn sub(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
         let difference = a - b;
         msg!("Difference is {}", difference);
         Ok(())
     }
 
-    pub fn mul(ctx: Context<Initialize>, a: u64, b: u64) -> Result<()> {
+    pub fn mul(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
         let result = a * b;
         msg!("Multipled is {}", result);
         Ok(())
     }
 
-    pub fn div(ctx: Context<Initialize>, a: f64, b: f64) -> Result<()> {
+    pub fn div(ctx: Context<BoatyMcBoatface >, a: f64, b: f64) -> Result<()> {
         let result = a / b;
         msg!("Divided is {}", result);
         Ok(())
     }
 
-    pub fn modulus(ctx: Context<Initialize>, a: u64, b: u64) -> Result<()> {
+    pub fn modulus(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
         let result = a % b;
         msg!("Modulus is {}", result);
         Ok(())
@@ -44,4 +44,4 @@ pub mod day3 {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct BoatyMcBoatface  {}
