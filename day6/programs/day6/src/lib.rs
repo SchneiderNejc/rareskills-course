575da9e7f517db3098e0daa85503438a83cf3278
Nejc Schneider <nejc.sch@gmail.com>
2024-08-22 14:35:59 +0200

    age_checker(): use match instead

--- day6/programs/day6/src/lib.rs
+++ day6/programs/day6/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5xJNXoJhRUCixjzuzEbvP1qK3H4TxHx9cCcuRVMimWQm");
+declare_id!("6jFD32aG6PwaYdLUgswa445xL7Lg3uKsLbApJv52CuNG");
 
 #[program]
 pub mod day6 {
@@ -8,9 +8,25 @@ pub mod day6 {
 
     pub fn age_checker(ctx: Context<Initialize>,
         age: u64) -> Result<()> {
-
-        let result = if age >= 18 {"You are 18 years old or above"} else { "You are below 18 years old" };
-        msg!("{:?}", result);
+    match age {
+        1 => {
+            // Code block executed if age equals 1
+            msg!("The age is 1");
+        },
+        2 | 3 => {
+            // Code block executed if age equals 2 or 3
+            msg!("The age is either 2 or 3");
+        },
+        4..=6 => {
+            // Code block executed if age is in the
+            // range 4 to 6 (inclusive)
+            msg!("The age is between 4 and 6");
+        },
+        _ => {
+            // Code block executed for any other age
+            msg!("The age is something else");
+        }
+        }
         Ok(())
     }
 
