72e0fa0e20ae5d7c918fc282df53c58162357d67
Nejc Schneider <nejc.sch@gmail.com>
2024-08-22 20:49:09 +0200

    test the mapping

--- day6/tests/day6.ts
+++ day6/tests/day6.ts
@@ -13,4 +13,10 @@ describe("day6", () => {
     const tx = await program.methods.ageChecker(new anchor.BN(35)).rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Prints mapping!", async () => {
+    // Add your test here.
+    const tx = await program.methods.mapping("name", "Bob").rpc();
+    console.log("Your transaction signature", tx);
+  });
 });
