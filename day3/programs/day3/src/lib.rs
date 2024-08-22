d8bc0569699c1ff88a8ca1d5b8d24350350c4b39
Nejc Schneider <nejc.sch@gmail.com>
2024-08-22 13:21:23 +0200

    day3: add Signer to context

--- day3/programs/day3/src/lib.rs
+++ day3/programs/day3/src/lib.rs
@@ -3,45 +3,23 @@ use anchor_lang::prelude::*;
 declare_id!("J4JFzPXckHctGsYtDR1gPueCoaXycNCJ3tei533XPB2");
 
 #[program]
-pub mod day3 {
+pub mod anchor_function_tutorial {
     use super::*;
 
-    pub fn boaty_mc_boatface(ctx: Context<BoatyMcBoatface >) -> Result<()> {
+    pub fn function_a(ctx: Context<NonEmptyAccountExample>) -> Result<()> {
         Ok(())
     }
 
-    pub fn add(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
-        let result: u64 = a + b;
-        msg!("Result of addition: {}", result);
+    pub fn function_b(ctx: Context<Empty>, firstArg: u64) -> Result<()> {
         Ok(())
     }
-
-    pub fn sub(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
-        let difference = a - b;
-        msg!("Difference is {}", difference);
-        Ok(())
-    }
-
-    pub fn mul(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
-        let result = a * b;
-        msg!("Multipled is {}", result);
-        Ok(())
-    }
-
-    pub fn div(ctx: Context<BoatyMcBoatface >, a: f64, b: f64) -> Result<()> {
-        let result = a / b;
-        msg!("Divided is {}", result);
-        Ok(())
-    }
-
-    pub fn modulus(ctx: Context<BoatyMcBoatface >, a: u64, b: u64) -> Result<()> {
-        let result = a % b;
-        msg!("Modulus is {}", result);
-        Ok(())
-    }
-
-
 }
 
 #[derive(Accounts)]
-pub struct BoatyMcBoatface  {}
+pub struct NonEmptyAccountExample<'info> {
+    signer: Signer<'info>,
+    another_signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Empty {}
