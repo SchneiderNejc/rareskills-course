6719349306c449ff897b83e8f9487383a139e60c
Nejc Schneider <nejc.sch@gmail.com>
2024-08-23 12:11:47 +0200

    add test prints struct

--- day6/tests/day6.ts
+++ day6/tests/day6.ts
@@ -14,6 +14,12 @@ describe("day6", () => {
     console.log("Your transaction signature", tx);
   });
 
+  // it("Prints struct.", async () => {
+  //   // Add your test here.
+  //   const tx = await program.methods.structure("Paul", new anchor.BN(64)).rpc();
+  //   console.log("Your transaction signature", tx);
+  // });
+
   it("Prints mapping!", async () => {
     // Add your test here.
     const tx = await program.methods.mapping("name", "Bob").rpc();
