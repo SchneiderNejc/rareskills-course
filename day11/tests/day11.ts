962653317978d61219126012aa07144eefa063d8
Nejc Schneider <nejc.sch@gmail.com>
2024-08-26 12:42:01 +0200

    init day11

---
+++ day11/tests/day11.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Day11 } from "../target/types/day11";
+
+describe("day11", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Day11 as Program<Day11>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
