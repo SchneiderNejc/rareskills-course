ff0d5367a073e694af4b2172a2deb471d7181aa3
Nejc Schneider <nejc.sch@gmail.com>
2024-08-26 12:52:13 +0200

    add get_day_of_the_week()

--- day11/programs/day11/src/lib.rs
+++ day11/programs/day11/src/lib.rs
@@ -5,14 +5,29 @@ declare_id!("6XS1mbtFU5T9yeWPLG1x51zyXxEuSqDu9ZaM8v8ZDRGJ");
 #[program]
 pub mod day11 {
     use super::*;
+    use chrono::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         let clock: Clock = Clock::get()?;
         msg!(
             "Block timestamp: {}",
             // Get block.timestamp
             clock.unix_timestamp,
         );
+        Ok(())
+    }
+
+    pub fn get_day_of_the_week(
+        _ctx: Context<Initialize>) -> Result<()> {
+
+        let clock = Clock::get()?;
+        let time_stamp = clock.unix_timestamp; // current timestamp
+
+        let date_time = chrono::NaiveDateTime::from_timestamp_opt(time_stamp, 0).unwrap();
+        let day_of_the_week = date_time.weekday();
+
+        msg!("Week day is: {}", day_of_the_week);
+
         Ok(())
     }
 }
