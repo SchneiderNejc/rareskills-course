63dfd5db1a06cf03761d1dc47c7e05536935efa8
Nejc Schneider <nejc.sch@gmail.com>
2024-08-26 12:49:52 +0200

    print block.timestamp

--- day11/programs/day11/src/lib.rs
+++ day11/programs/day11/src/lib.rs
@@ -1,12 +1,18 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("6XS1mbtFU5T9yeWPLG1x51zyXxEuSqDu9ZaM8v8ZDRGJ");
 
 #[program]
 pub mod day11 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let clock: Clock = Clock::get()?;
+        msg!(
+            "Block timestamp: {}",
+            // Get block.timestamp
+            clock.unix_timestamp,
+        );
         Ok(())
     }
 }
