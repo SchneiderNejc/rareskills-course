85151ed502bc3c837b7b998921056fd366bf016b
Nejc Schneider <nejc.sch@gmail.com>
2024-08-27 11:37:09 +0200

    EpochSchedule sysvar

--- day12/programs/day12/src/lib.rs
+++ day12/programs/day12/src/lib.rs
@@ -7,14 +7,19 @@ pub mod day12 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // ------------------- Clock sysvar -------------------
         // Get the Clock sysvar
         let clock = Clock::get()?;
 
-        msg!(
-            "clock: {:?}",
-            // Retrieve all the details of the Clock sysvar
-            clock
-        );
+        // Log the details of the Clock sysvar
+        msg!("clock: {:?}", clock);
+
+        // ------------------- EpochSchedule sysvar -------------------
+        // Get the EpochSchedule sysvar
+        let epoch_schedule = EpochSchedule::get()?;
+
+        // Log the details of the EpochSchedule sysvar
+        msg!("EpochSchedule: {:?}", epoch_schedule);
 
         Ok(())
     }
