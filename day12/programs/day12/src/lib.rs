1b302b15228d02e4a31257e9f314980ec16c398c
Nejc Schneider <nejc.sch@gmail.com>
2024-08-27 15:55:07 +0200

    add comment about Address-dependend-sysvars

--- day12/programs/day12/src/lib.rs
+++ day12/programs/day12/src/lib.rs
@@ -1,29 +1,35 @@
 use anchor_lang::prelude::*;
 
-declare_id!("FK7sdzQhjoetR2oFDdLEyWfkechXFAfueUmy1L51Ldb4");
+declare_id!("2Kcrje5YbJVskthgEJVqzDcbAAu8CL2PuvoJiM6xpDJr");
 
 #[program]
 pub mod day12 {
     use super::*;
+    // Following import is for the public-address-specific sysvars
+    // use anchor_lang::solana_program::sysvar::{instructions, fees::Fees, recent_blockhashes::RecentBlockhashes};
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        // ------------------- Clock sysvar -------------------
-        // Get the Clock sysvar
+        // Clock sysvar
         let clock = Clock::get()?;
-
-        // Log the details of the Clock sysvar
         msg!("clock: {:?}", clock);
 
-        // ------------------- EpochSchedule sysvar -------------------
-        // Get the EpochSchedule sysvar
+        // EpochSchedule sysvar
         let epoch_schedule = EpochSchedule::get()?;
-
-        // Log the details of the EpochSchedule sysvar
         msg!("EpochSchedule: {:?}", epoch_schedule);
 
         // Rent sysvar
         let rent_var = Rent::get()?;
         msg!("Rent {:?}", rent_var);
+
+        // ------------------- Accessing Sysvars Using Sysvar Public Address -------------------
+
+        // Sysvars we didn't test due changing pub struct Initialize
+        // StakeHistory, RecentBlockhashes, Fees, Instruction, LastRestartSlot
+
+/*      In the current version of Anchor, it is not feasible to access certain sysvars.
+        These sysvars include EpochRewards, SlotHistory, and SlotHashes.
+        When attempting to access these sysvars, it results to an error. */
+
         Ok(())
     }
 }
