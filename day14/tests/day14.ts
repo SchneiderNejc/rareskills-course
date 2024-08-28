6e4776fa744c42088c715a83c9cdfe095731349a
Nejc Schneider <nejc.sch@gmail.com>
2024-08-28 15:20:40 +0200

    test for two_signers()

--- day14/tests/day14.ts
+++ day14/tests/day14.ts
@@ -19,4 +19,22 @@ describe("day14", () => {
 
     console.log("The signer1: ", program.provider.publicKey.toBase58());
   });
+
+  it("Is signed by multiple signers", async () => {
+    // generate a signer to call our function
+    let myKeypair = anchor.web3.Keypair.generate();
+
+    // Add your test here.
+    const tx = await program.methods
+      .twoSigners()
+      .accounts({
+        signer1: program.provider.publicKey,
+        signer2: myKeypair.publicKey,
+      })
+      .signers([myKeypair])
+      .rpc();
+
+    console.log("The signer1: ", program.provider.publicKey.toBase58());
+    console.log("The signer2: ", myKeypair.publicKey.toBase58());
+  });
 });
