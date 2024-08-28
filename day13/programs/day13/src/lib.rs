55627fcecca31d809bbed02685396d5589bf1721
Nejc Schneider <nejc.sch@gmail.com>
2024-08-28 11:43:59 +0200

    add events

--- day13/programs/day13/src/lib.rs
+++ day13/programs/day13/src/lib.rs
@@ -7,9 +7,22 @@ pub mod day13 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        emit!(MyEvent { value: 42 });
+        emit!(MySecondEvent { value: 3, message: "hello world".to_string() });
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[event]
+pub struct MyEvent {
+    pub value: u64,
+}
+
+#[event]
+pub struct MySecondEvent {
+		pub value: u64,
+    pub message: String,
+}
