2d73b76a3cc19ac22dbcf9b876dbea0c12b772ed
Nejc Schneider <nejc.sch@gmail.com>
2024-08-28 14:37:07 +0200

    define signer (tx.origin)

--- day14/programs/day14/src/lib.rs
+++ day14/programs/day14/src/lib.rs
@@ -1,15 +1,22 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("FinKTpyeRA8as4H4jbfKKnbcaj94Lwiz9FJxd391DwKk");
 
 #[program]
 pub mod day14 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        // @dev Signer doesen't have tu be mutable.
+        let the_signer1: &mut Signer = &mut ctx.accounts.signer1;
+
+        msg!("The signer1: {:?}", *the_signer1.key);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(mut)]
+    pub signer1: Signer<'info>,
+}
