e36f4bc5588821555b6fe3a95af16989ac669a21
Nejc Schneider <nejc.sch@gmail.com>
2024-09-01 17:30:17 +0200

    test set()

--- day17/tests/day17.ts
+++ day17/tests/day17.ts
@@ -8,9 +8,32 @@ describe("day17", () => {
 
   const program = anchor.workspace.Day17 as Program<Day17>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  xit("Is initialized!", async () => {
+    // Calculate account address that stores MyStruct.
+    const seeds = [];
+    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
+
+    console.log("the storage account address is", myStorage.toBase58());
+
+    // Pass address to initialize().
+    await program.methods.initialize().accounts({ myStorage: myStorage }).rpc();
+  });
+
+  it("Can set value.", async () => {
+    const seeds = [];
+    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
+
+    console.log("the storage account address is", myStorage.toBase58());
+
+    await program.methods
+      .set(new anchor.BN(170))
+      .accounts({ myStorage: myStorage })
+      .rpc();
   });
 });
