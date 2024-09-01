b44bbdd6f7f4c36f7f811cffb73a0f2b45ff484f
Nejc Schneider <nejc.sch@gmail.com>
2024-09-01 13:02:49 +0200

    different data types have different cost

--- day15/programs/day15/src/lib.rs
+++ day15/programs/day15/src/lib.rs
@@ -7,7 +7,52 @@ pub mod day15 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
+    // this costs 600 CU (type defaults to Vec<i32>)
+    let mut a = Vec::new();
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+
+    // this costs 618 CU
+    let mut a: Vec<u64> = Vec::new();
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+
+    // this costs 600 CU (same as the first one but the type was explicitly denoted)
+    let mut a: Vec<i32> = Vec::new();
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+
+    // this costs 618 CU (takes the same space as u64)
+    let mut a: Vec<i64> = Vec::new();
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+
+    // this costs 459 CU
+    let mut a: Vec<u8> = Vec::new();
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+    a.push(1);
+
+    Ok(())
     }
 }
 
