6949e508e16ada19b2511ffd8ac8368262bb03e3
Nejc Schneider <nejc.sch@gmail.com>
2024-09-04 12:26:34 +0200

    update tests for nested mapping

--- day19/tests/day19.ts
+++ day19/tests/day19.ts
@@ -11,23 +11,32 @@ describe("day19", () => {
   // @dev Will only work once. For consequtive tests
   // restart the client with solana-test-validator --reset
   it("Initialize and set value", async () => {
-    const key = new anchor.BN(42);
+    // we now have two keys
+    const key1 = new anchor.BN(42);
+    const key2 = new anchor.BN(43);
     const value = new anchor.BN(1337);
 
-    const seeds = [key.toArrayLike(Buffer, "le", 8)];
+    // seeds has two values
+    const seeds = [
+      key1.toArrayLike(Buffer, "le", 8),
+      key2.toArrayLike(Buffer, "le", 8),
+    ];
     let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
       seeds,
       program.programId
     )[0];
 
-    await program.methods.initialize(key).accounts({ val: valueAccount }).rpc();
-
-    // set the account
-    await program.methods.set(key, value).accounts({ val: valueAccount }).rpc();
+    await program.methods
+      .initialize(key1, key2)
+      .accounts({ val: valueAccount })
+      .rpc();
+    await program.methods
+      .set(key1, key2, value)
+      .accounts({ val: valueAccount })
+      .rpc();
 
     // read the account back
     let result = await program.account.val.fetch(valueAccount);
-
     console.log(
       `the value ${result.value} was stored in ${valueAccount.toBase58()}`
     );
