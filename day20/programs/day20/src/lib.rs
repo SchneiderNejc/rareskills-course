77263fcfdff226e1319f48c7cf643576f1b5a1ab
Nejc Schneider <nejc.sch@gmail.com>
2024-09-04 15:34:14 +0200

    calculate rent of an empty account

--- day20/programs/day20/src/lib.rs
+++ day20/programs/day20/src/lib.rs
@@ -7,6 +7,13 @@ pub mod day20 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let cost_of_empty_acc = rent_module::ACCOUNT_STORAGE_OVERHEAD as f64 *
+                                rent_module::DEFAULT_LAMPORTS_PER_BYTE_YEAR as f64 *
+                                rent_module::DEFAULT_EXEMPTION_THRESHOLD;
+
+        msg!("cost to create an empty account: {}", cost_of_empty_acc);
+        // 890880
+
         Ok(())
     }
 }
