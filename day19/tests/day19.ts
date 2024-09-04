95595cf8c65ff49660e6b5e005d355b14f61a0f8
Nejc Schneider <nejc.sch@gmail.com>
2024-09-04 14:55:16 +0200

    test 2 independent mappings

--- day19/tests/day19.ts
+++ day19/tests/day19.ts
@@ -14,33 +14,64 @@ describe("day19", () => {
     // we now have two keys
     const key1 = new anchor.BN(42);
     const key2 = new anchor.BN(43);
-    const key3 = new anchor.BN(44);
+    const key = new anchor.BN(44);
     const value = new anchor.BN(1337);
 
-    // seeds has two values
-    const seeds = [
+    // Seeds
+    const seedsMapping1 = [
+      Buffer.from("mapping1"), // Distinct identifier for the first mapping
       key1.toArrayLike(Buffer, "le", 8),
       key2.toArrayLike(Buffer, "le", 8),
-      key3.toArrayLike(Buffer, "le", 8),
     ];
-    let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
-      seeds,
+    const seedsMapping2 = [
+      Buffer.from("mapping2"), // Distinct identifier for the second mapping
+      key.toArrayLike(Buffer, "le", 8),
+    ];
+    // Values
+    let valueAccountMapping1 = anchor.web3.PublicKey.findProgramAddressSync(
+      seedsMapping1,
+      program.programId
+    )[0];
+
+    let valueAccountMapping2 = anchor.web3.PublicKey.findProgramAddressSync(
+      seedsMapping2,
       program.programId
     )[0];
 
     await program.methods
-      .initialize(key1, key2, key3)
-      .accounts({ val: valueAccount })
-      .rpc();
-    await program.methods
-      .set(key1, key2, key3, value)
-      .accounts({ val: valueAccount })
+      .initialize(key1, key2)
+      .accounts({ val: valueAccountMapping1 })
       .rpc();
 
-    // read the account back
-    let result = await program.account.val.fetch(valueAccount);
+    await program.methods
+      .initializeSecond(key)
+      .accounts({ val: valueAccountMapping2 })
+      .rpc();
+
+    await program.methods
+      .set(key1, key2, value)
+      .accounts({ val: valueAccountMapping1 })
+      .rpc();
+
+    await program.methods
+      .setSecond(key, value)
+      .accounts({ val: valueAccountMapping2 })
+      .rpc();
+
+    // read the account back for mapping1
+    let resultMapping1 = await program.account.val.fetch(valueAccountMapping1);
     console.log(
-      `the value ${result.value} was stored in ${valueAccount.toBase58()}`
+      `the value ${
+        resultMapping1.value
+      } was stored in ${valueAccountMapping1.toBase58()}`
+    );
+
+    // read the account back for mapping2
+    let resultMapping2 = await program.account.val.fetch(valueAccountMapping2);
+    console.log(
+      `the value ${
+        resultMapping2.value
+      } was stored in ${valueAccountMapping2.toBase58()}`
     );
   });
 });
