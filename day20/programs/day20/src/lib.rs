fc33c4729548f30f923dda928a53dbd681e47457
Nejc Schneider <nejc.sch@gmail.com>
2024-09-04 15:35:09 +0200

    calcualte rent for non-empty account

--- day20/programs/day20/src/lib.rs
+++ day20/programs/day20/src/lib.rs
@@ -12,8 +12,15 @@ pub mod day20 {
                                 rent_module::DEFAULT_EXEMPTION_THRESHOLD;
 
         msg!("cost to create an empty account: {}", cost_of_empty_acc);
-        // 890880
+        // 890,880 lamports
 
+        let cost_for_32_bytes = cost_of_empty_acc +
+                                32 as f64 *
+                                rent_module::DEFAULT_LAMPORTS_PER_BYTE_YEAR as f64 *
+                                rent_module::DEFAULT_EXEMPTION_THRESHOLD;
+
+        msg!("cost to create a 32 byte account: {}", cost_for_32_bytes);
+        // 1,113,600 lamports
         Ok(())
     }
 }
