4f9f8a1a689d2feedd35d36aedceea06c34cec1d
Nejc Schneider <nejc.sch@gmail.com>
2024-09-04 14:55:07 +0200

    2 independent mappings

--- day19/programs/day19/src/lib.rs
+++ day19/programs/day19/src/lib.rs
@@ -7,44 +7,84 @@ declare_id!("AfD7NnFyUnHfoS8kvhK5MTvuLqEhVggEKnJmpKuKM4qG");
 pub mod day19 {
     use super::*;
 
-    pub fn initialize(_ctx: Context<Initialize>, _key1: u64, _key2: u64, _key3: u64) -> Result<()> {
+    pub fn initialize(_ctx: Context<Initialize>, key1: u64, key2: u64) -> Result<()> {
         Ok(())
     }
 
-    pub fn set(ctx: Context<Set>, _key1: u64, _key2: u64, _key3: u64, val: u64) -> Result<()> {
+    pub fn initialize_second(ctx: Context<InitializeSecondMapping>, key: u64) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn set(ctx: Context<Set>, key1: u64, key2: u64, val: u64) -> Result<()> {
         ctx.accounts.val.value = val;
         Ok(())
     }
 
+    pub fn set_second(ctx: Context<SetSecondMapping>, key: u64, val: u64) -> Result<()> {
+        ctx.accounts.val.value = val;
+        Ok(())
+    }
 
 }
 
-#[derive(Accounts)]
-#[instruction(key1: u64, key2: u64, key3: u64)] // new key args added
-pub struct Set<'info> {
-    #[account(mut)]
-    val: Account<'info, Val>,
-}
+
 
 #[derive(Accounts)]
-#[instruction(key1: u64, key2: u64, key3: u64)] // new key args added
+#[instruction(key1: u64, key2: u64)]
 pub struct Initialize<'info> {
-
     #[account(init,
         payer = signer,
         space = size_of::<Val>() + 8,
         seeds=[
-                &key1.to_le_bytes().as_ref(),
-                &key2.to_le_bytes().as_ref(),
-                &key3.to_le_bytes().as_ref()
-            ], // 3 seeds
+                b"mapping1",  // Distinct identifier for the first mapping
+                &key1.to_le_bytes(),
+                &key2.to_le_bytes()
+            ],
         bump)]
-val: Account<'info, Val>,
+    val: Account<'info, Val>,
 
     #[account(mut)]
     signer: Signer<'info>,
 
     system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(key: u64)]
+pub struct InitializeSecondMapping<'info> {
+    #[account(init,
+        payer = signer,
+        space = size_of::<Val>() + 8,
+        seeds=[
+                b"mapping2",  // Distinct identifier for the second mapping
+                &key.to_le_bytes()
+            ],
+        bump)]
+    val: Account<'info, Val>,
+
+    #[account(mut)]
+    signer: Signer<'info>,
+
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(key1: u64, key2: u64)]
+pub struct Set<'info> {
+    #[account(mut, seeds=[
+        b"mapping1",
+        &key1.to_le_bytes(),
+        &key2.to_le_bytes()],
+        bump
+    )]
+    val: Account<'info, Val>,
+}
+
+#[derive(Accounts)]
+#[instruction(key: u64)]
+pub struct SetSecondMapping<'info> {
+    #[account(mut, seeds=[b"mapping2", &key.to_le_bytes()], bump)]
+    val: Account<'info, Val>,
 }
 
 #[account]
