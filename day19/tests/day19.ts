99ca8eed9ef123e1b5e1cc95d1e128d56ede635c
Nejc Schneider <nejc.sch@gmail.com>
2024-09-04 11:14:27 +0200

    test init and set mapping

--- day19/tests/day19.ts
+++ day19/tests/day19.ts
@@ -8,15 +8,28 @@ describe("day19", () => {
 
   const program = anchor.workspace.Day19 as Program<Day19>;
 
-  it("Initialize mapping storage", async () => {
+  // @dev Will only work once. For consequtive tests
+  // restart the client with solana-test-validator --reset
+  it("Initialize and set value", async () => {
     const key = new anchor.BN(42);
+    const value = new anchor.BN(1337);
 
     const seeds = [key.toArrayLike(Buffer, "le", 8)];
-    const [valueAccount, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+    let valueAccount = anchor.web3.PublicKey.findProgramAddressSync(
       seeds,
       program.programId
-    );
+    )[0];
 
     await program.methods.initialize(key).accounts({ val: valueAccount }).rpc();
+
+    // set the account
+    await program.methods.set(key, value).accounts({ val: valueAccount }).rpc();
+
+    // read the account back
+    let result = await program.account.val.fetch(valueAccount);
+
+    console.log(
+      `the value ${result.value} was stored in ${valueAccount.toBase58()}`
+    );
   });
 });
