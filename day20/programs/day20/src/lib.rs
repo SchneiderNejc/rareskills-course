5d7a64c2acbbccb2c3f141a9db794873f632155f
Nejc Schneider <nejc.sch@gmail.com>
2024-09-05 12:47:19 +0200

    Change size of an account

--- day20/programs/day20/src/lib.rs
+++ day20/programs/day20/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::rent as rent_module;
+use std::mem::size_of;
 
 
 declare_id!("5QpKqiLQkgDmkJCQETbN8Un1QDKRsmigm5wNsQy4i3DE");
@@ -9,23 +9,50 @@ pub mod day20 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        let cost_of_empty_acc = rent_module::ACCOUNT_STORAGE_OVERHEAD as f64 *
-                                rent_module::DEFAULT_LAMPORTS_PER_BYTE_YEAR as f64 *
-                                rent_module::DEFAULT_EXEMPTION_THRESHOLD;
+        Ok(())
+    }
 
-        msg!("cost to create an empty account: {}", cost_of_empty_acc);
-        // 890,880 lamports
-
-        let cost_for_32_bytes = cost_of_empty_acc +
-                                32 as f64 *
-                                rent_module::DEFAULT_LAMPORTS_PER_BYTE_YEAR as f64 *
-                                rent_module::DEFAULT_EXEMPTION_THRESHOLD;
-
-        msg!("cost to create a 32 byte account: {}", cost_for_32_bytes);
-        // 1,113,600 lamports
+    pub fn increase_account_size(ctx: Context<IncreaseAccountSize>) -> Result<()> {
         Ok(())
     }
 }
 
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct IncreaseAccountSize<'info> {
+
+    #[account(mut,
+							// ***** 1,000 BYTE INCREMENT IS OVER HERE *****
+              realloc = size_of::<MyStorage>() + 8 + 1000, // @dev Maximum is 10240.
+              realloc::payer = signer,
+              realloc::zero = false, // @dev If set to true, account data is erased (set to all zeros).
+              seeds = [],
+              bump)]
+    pub my_storage: Account<'info, MyStorage>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+
+    #[account(init,
+              payer = signer,
+              space=size_of::<MyStorage>() + 8,
+              seeds = [],
+              bump)]
+    pub my_storage: Account<'info, MyStorage>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MyStorage {
+    x: u64,
+}
