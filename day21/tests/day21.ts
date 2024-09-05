1d39abf694b7320255f6b1745e1fb387d055390d
Nejc Schneider <nejc.sch@gmail.com>
2024-09-05 13:08:16 +0200

    test read_balance()

--- day21/tests/day21.ts
+++ day21/tests/day21.ts
@@ -5,12 +5,17 @@ import { Day21 } from "../target/types/day21";
 describe("day21", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.Day21 as Program<Day21>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // the following is the Solana wallet we are using
+  let pubkey = new anchor.web3.PublicKey(
+    "6ZJDfSVjffvRYbtpFF33PSeYWNYJnbtMKiwWVt1YRjZW"
+  );
+
+  it("Tests the balance", async () => {
+    const tx = await program.methods
+      .readBalance()
+      .accounts({ acct: pubkey })
+      .rpc();
   });
 });
