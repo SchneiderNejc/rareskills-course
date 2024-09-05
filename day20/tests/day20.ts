0d3443e21e72795ab001d2b3ab64de48472f9870
Nejc Schneider <nejc.sch@gmail.com>
2024-09-05 12:47:53 +0200

    test change size of an account

--- day20/tests/day20.ts
+++ day20/tests/day20.ts
@@ -8,9 +8,35 @@ describe("day20", () => {
 
   const program = anchor.workspace.Day20 as Program<Day20>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // ------------------- Helper functions -------------------
+  // @todo store myStorage in global var and only fetch and print it once.
+  function getMyStorageAddress() {
+    const seeds = [];
+    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
+
+    console.log("The storage account address is", myStorage.toBase58());
+
+    return myStorage;
+  }
+
+  // ------------------- Tests -------------------
+  // @note Only run once.
+  xit("Is initialized!", async () => {
+    const myStorage = getMyStorageAddress();
+
+    await program.methods.initialize().accounts({ myStorage: myStorage }).rpc();
+  });
+
+  // @notice Verify account size with solana account [address]
+  it("Increased account size.", async () => {
+    const myStorage = getMyStorageAddress();
+
+    await program.methods
+      .increaseAccountSize()
+      .accounts({ myStorage: myStorage })
+      .rpc();
   });
 });
