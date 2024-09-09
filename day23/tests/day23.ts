401b10f7fb948020249e0eb2208374eeb1971743
Nejc Schneider <nejc.sch@gmail.com>
2024-09-09 10:45:32 +0200

    test send_sol

--- day23/tests/day23.ts
+++ day23/tests/day23.ts
@@ -8,13 +8,24 @@ describe("day23", () => {
 
   const program = anchor.workspace.Day23 as Program<Day23>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
   async function printAccountBalance(account) {
     const balance = await anchor.getProvider().connection.getBalance(account);
     console.log(`${account} has ${balance / anchor.web3.LAMPORTS_PER_SOL} SOL`);
   }
+
+  it("Transmit SOL", async () => {
+    // generate a new wallet
+    const recipient = anchor.web3.Keypair.generate();
+
+    await printAccountBalance(recipient.publicKey);
+
+    // send the account 1 SOL via the program
+    let amount = new anchor.BN(1 * anchor.web3.LAMPORTS_PER_SOL);
+    await program.methods
+      .sendSol(amount)
+      .accounts({ recipient: recipient.publicKey })
+      .rpc();
+
+    await printAccountBalance(recipient.publicKey);
   });
 });
