66329355570b97df8f14c2525d43aa43f9a5b445
Nejc Schneider <nejc.sch@gmail.com>
2024-09-09 14:01:04 +0200

    add split_sol()

--- day23/programs/day23/src/lib.rs
+++ day23/programs/day23/src/lib.rs
@@ -55,6 +55,31 @@ pub mod day23 {
 
         Ok(())
     }
+    // 'a, 'b, 'c are Rust lifetimes, ignore them for now
+    pub fn split_sol<'a, 'b, 'c, 'info>(
+        ctx: Context<'a, 'b, 'c, 'info, SplitSol<'info>>,
+        amount: u64,
+    ) -> Result<()> {
+
+        let amount_each_gets = amount / ctx.remaining_accounts.len() as u64;
+        let system_program = &ctx.accounts.system_program;
+
+				// note the keyword `remaining_accounts`
+        for recipient in ctx.remaining_accounts {
+            let cpi_accounts = system_program::Transfer {
+                from: ctx.accounts.signer.to_account_info(),
+                to: recipient.to_account_info(),
+            };
+            let cpi_program = system_program.to_account_info();
+            let cpi_context = CpiContext::new(cpi_program, cpi_accounts);
+
+            let res = system_program::transfer(cpi_context, amount_each_gets);
+            if !res.is_ok() {
+                return err!(Errors::TransferFailed);
+            }
+        }
+        Ok(())
+    }
 }
 
 #[error_code]
@@ -90,4 +115,11 @@ pub struct SplitSendSol<'info> {
 
     #[account(mut)]
     signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct SplitSol<'info> {
+    #[account(mut)]
+    signer: Signer<'info>,
+    system_program: Program<'info, System>,
 }
