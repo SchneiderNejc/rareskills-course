1721233862f94e1e662a796469130fda3a6b9f81
Nejc Schneider <nejc.sch@gmail.com>
2024-09-10 16:52:43 +0200

    init PDA account (traditional)

--- day25/programs/day25/src/lib.rs
+++ day25/programs/day25/src/lib.rs
@@ -1,15 +1,35 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("7YnuL66xr2JNEwS7Tq62Pf6jxvWpg9gMM6hDLHbZBRDr");
 
 #[program]
 pub mod day25 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_pda(ctx: Context<InitializePDA>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializePDA<'info> {
+
+    // This is the program derived address
+    #[account(init,
+              payer = signer,
+              space=size_of::<MyPDA>() + 8,
+              seeds = [],
+              bump)]
+    pub my_pda: Account<'info, MyPDA>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MyPDA {
+    x: u64,
+}
