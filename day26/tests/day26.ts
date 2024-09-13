ce31a156d00e657cbeb3d69b06413451aa1fd7f6
Nejc Schneider <nejc.sch@gmail.com>
2024-09-13 10:45:21 +0200

    Tests: Helper functions

--- day26/tests/day26.ts
+++ day26/tests/day26.ts
@@ -2,6 +2,29 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Day26 } from "../target/types/day26";
 
+// ------------------ Helper functions ------------------
+async function airdropSol(publicKey, amount) {
+  let airdropTx = await anchor
+    .getProvider()
+    .connection.requestAirdrop(
+      publicKey,
+      amount * anchor.web3.LAMPORTS_PER_SOL
+    );
+  await confirmTransaction(airdropTx);
+}
+
+async function confirmTransaction(tx) {
+  const latestBlockHash = await anchor
+    .getProvider()
+    .connection.getLatestBlockhash();
+  await anchor.getProvider().connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: tx,
+  });
+}
+
+// ------------------ Test functions ------------------
 describe("day26", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
