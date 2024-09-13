6518fafbaf211b7f6ec1b0df5c981780139d42ac
Nejc Schneider <nejc.sch@gmail.com>
2024-09-13 10:45:41 +0200

    test init keypair and init pda

--- day26/tests/day26.ts
+++ day26/tests/day26.ts
@@ -32,8 +32,54 @@ describe("day26", () => {
   const program = anchor.workspace.Day26 as Program<Day26>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log("program address", program.programId.toBase58());
+    const seeds = [];
+    const [pda, bump_] = anchor.web3.PublicKey.findProgramAddressSync(
+      seeds,
+      program.programId
+    );
+
+    console.log(
+      "owner of pda before initialize:",
+      await anchor.getProvider().connection.getAccountInfo(pda)
+    );
+
+    await program.methods.initializePda().accounts({ pda: pda }).rpc();
+
+    console.log(
+      "owner of pda after initialize:",
+      (
+        await anchor.getProvider().connection.getAccountInfo(pda)
+      ).owner.toBase58()
+    );
+
+    let keypair = anchor.web3.Keypair.generate();
+
+    console.log(
+      "owner of keypair before airdrop:",
+      await anchor.getProvider().connection.getAccountInfo(keypair.publicKey)
+    );
+
+    await airdropSol(keypair.publicKey, 1); // 1 SOL
+
+    console.log(
+      "owner of keypair after airdrop:",
+      (
+        await anchor.getProvider().connection.getAccountInfo(keypair.publicKey)
+      ).owner.toBase58()
+    );
+
+    await program.methods
+      .initializeKeypair()
+      .accounts({ keypair: keypair.publicKey })
+      .signers([keypair]) // the signer must be the keypair
+      .rpc();
+
+    console.log(
+      "owner of keypair after initialize:",
+      (
+        await anchor.getProvider().connection.getAccountInfo(keypair.publicKey)
+      ).owner.toBase58()
+    );
   });
 });
