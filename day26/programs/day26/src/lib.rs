5c69f54f94ed5b5363a826a1a8982dbd701daa8e
Nejc Schneider <nejc.sch@gmail.com>
2024-09-13 10:44:34 +0200

    add initialize_pda() and initialize_keypair()

--- day26/programs/day26/src/lib.rs
+++ day26/programs/day26/src/lib.rs
@@ -1,15 +1,40 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("4RgrgZq6zuSggZtsAyYKgRhkuXDBa2VFvb8Bw7uQK5Wg");
 
 #[program]
 pub mod day26 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_keypair(ctx: Context<InitializeKeypair>) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn initialize_pda(ctx: Context<InitializePda>) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeKeypair<'info> {
+    #[account(init, payer = signer, space = 8)]
+    keypair: Account<'info, Keypair>,
+    #[account(mut)]
+    signer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializePda<'info> {
+    #[account(init, payer = signer, space = 8, seeds = [], bump)]
+    pda: Account<'info, Pda>,
+    #[account(mut)]
+    signer: Signer<'info>,
+    system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Keypair();
+
+#[account]
+pub struct Pda();
