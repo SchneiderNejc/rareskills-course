9cbe6009b79badde5ea3bddbd966503d1bd5d268
Nejc Schneider <nejc.sch@gmail.com>
2024-09-13 15:28:07 +0200

    update code into crowdfund app with withdraw()

--- day26/programs/day26/src/lib.rs
+++ day26/programs/day26/src/lib.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
 use std::mem::size_of;
 use anchor_lang::system_program;
+use std::str::FromStr;
+
 
 declare_id!("4RgrgZq6zuSggZtsAyYKgRhkuXDBa2VFvb8Bw7uQK5Wg");
 
@@ -8,89 +10,67 @@ declare_id!("4RgrgZq6zuSggZtsAyYKgRhkuXDBa2VFvb8Bw7uQK5Wg");
 pub mod day26 {
     use super::*;
 
-    pub fn initialize_keypair(ctx: Context<InitializeKeypair>) -> Result<()> {
-        Ok(())
-    }
-
-    pub fn initialize_pda(ctx: Context<InitializePda>) -> Result<()> {
-        Ok(())
-    }
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let initialized_pda = &mut ctx.accounts.pda;
         Ok(())
     }
 
-    pub fn change_owner(ctx: Context<ChangeOwner>) -> Result<()> {
-        let account_info = &mut ctx.accounts.my_storage.to_account_info();
+    pub fn donate(ctx: Context<Donate>, amount: u64) -> Result<()> {
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.signer.to_account_info().clone(),
+                to: ctx.accounts.pda.to_account_info().clone(),
+            },
+        );
 
-				// assign is the function to transfer ownership
-				account_info.assign(&system_program::ID);
+        system_program::transfer(cpi_context, amount)?;
 
-				// we must erase all the data in the account or the transfer will fail
-        let res = account_info.realloc(0, false);
+        Ok(())
+    }
 
-        if !res.is_ok() {
-            return err!(Err::ReallocFailed);
-        }
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        // in anchor 0.29 or higher, use the following syntax:
+        // ctx.accounts.pda.sub_lamports(amount)?;
+        // ctx.accounts.signer.add_lamports(amount)?;
+
+
+        **ctx.accounts.pda.to_account_info().try_borrow_mut_lamports()? -= amount;
+        **ctx.accounts.signer.to_account_info().try_borrow_mut_lamports()? += amount;
         Ok(())
     }
 }
 
 
-
-#[error_code]
-pub enum Err {
-    #[msg("realloc failed")]
-    ReallocFailed,
-}
-
-#[derive(Accounts)]
-pub struct InitializeKeypair<'info> {
-    #[account(init, payer = signer, space = 8)]
-    keypair: Account<'info, Keypair>,
-    #[account(mut)]
-    signer: Signer<'info>,
-    system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct InitializePda<'info> {
-    #[account(init, payer = signer, space = 8, seeds = [], bump)]
-    pda: Account<'info, Pda>,
-    #[account(mut)]
-    signer: Signer<'info>,
-    system_program: Program<'info, System>,
-}
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-
-    #[account(init,
-              payer = signer,
-              space=size_of::<MyStorage>() + 8,
-              seeds = [],
-              bump)]
-    pub my_storage: Account<'info, MyStorage>,
-
     #[account(mut)]
     pub signer: Signer<'info>,
 
+    #[account(init, payer = signer, space=size_of::<Pda>() + 8, seeds=[], bump)]
+    pub pda: Account<'info, Pda>,
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct ChangeOwner<'info> {
+pub struct Donate<'info> {
     #[account(mut)]
-    pub my_storage: Account<'info, MyStorage>,
+    pub signer: Signer<'info>,
+
+    #[account(mut)]
+    pub pda: Account<'info, Pda>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Withdraw<'info> {
+    #[account(mut, address = Pubkey::from_str("5jmigjgt77kAfKsHri3MHpMMFPo6UuiAMF19VdDfrrTj").unwrap())]
+    pub signer: Signer<'info>,
+
+    #[account(mut)]
+    pub pda: Account<'info, Pda>,
 }
 
 #[account]
-pub struct MyStorage {
-    x: u64,
-}
-
-#[account]
-pub struct Keypair();
-
-#[account]
-pub struct Pda();
+pub struct Pda {}
