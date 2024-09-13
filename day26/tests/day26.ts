15cf6716e48affbfe678230323463d229c9bda2f
Nejc Schneider <nejc.sch@gmail.com>
2024-09-13 21:42:55 +0200

    update tests for crowdfunder app

--- day26/tests/day26.ts
+++ day26/tests/day26.ts
@@ -2,31 +2,6 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Day26 } from "../target/types/day26";
 
-import privateKey from "/home/nejc/.config/solana/id.json";
-// ------------------ Helper functions ------------------
-async function airdropSol(publicKey, amount) {
-  let airdropTx = await anchor
-    .getProvider()
-    .connection.requestAirdrop(
-      publicKey,
-      amount * anchor.web3.LAMPORTS_PER_SOL
-    );
-  await confirmTransaction(airdropTx);
-}
-
-async function confirmTransaction(tx) {
-  const latestBlockHash = await anchor
-    .getProvider()
-    .connection.getLatestBlockhash();
-  await anchor.getProvider().connection.confirmTransaction({
-    blockhash: latestBlockHash.blockhash,
-    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-    signature: tx,
-  });
-}
-
-// ------------------ Test functions ------------------
-// @notice init can only be run once, further runs will fail.
 describe("day26", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
@@ -34,93 +9,46 @@ describe("day26", () => {
   const program = anchor.workspace.Day26 as Program<Day26>;
 
   it("Is initialized!", async () => {
-    console.log("program address", program.programId.toBase58());
+    const programId = await program.account.pda.programId;
 
-    // ------------------ PDA Section ------------------
-    const seeds = [];
-    const [pda, bump_] = anchor.web3.PublicKey.findProgramAddressSync(
+    let seeds = [];
+    let pdaAccount = anchor.web3.PublicKey.findProgramAddressSync(
       seeds,
-      program.programId
-    );
+      programId
+    )[0];
 
-    console.log(
-      "owner of pda before airdrop:",
-      await anchor.getProvider().connection.getAccountInfo(pda)
-    );
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        pda: pdaAccount,
+      })
+      .rpc();
 
-    await airdropSol(pda, 1); // 1 SOL
-
-    console.log(
-      "owner of pda before airdrop:",
-      (
-        await anchor.getProvider().connection.getAccountInfo(pda)
-      ).owner.toBase58()
-    );
-
-    await program.methods.initializePda().accounts({ pda: pda }).rpc();
-
-    console.log(
-      "owner of pda after initialize:",
-      (
-        await anchor.getProvider().connection.getAccountInfo(pda)
-      ).owner.toBase58()
-    );
-
-    // ------------------ Keypair Section ------------------
-    let keypair = anchor.web3.Keypair.generate();
-
-    console.log(
-      "owner of keypair before airdrop:",
-      await anchor.getProvider().connection.getAccountInfo(keypair.publicKey)
-    );
-
-    await airdropSol(keypair.publicKey, 1); // 1 SOL
-
-    console.log(
-      "owner of keypair after airdrop:",
-      (
-        await anchor.getProvider().connection.getAccountInfo(keypair.publicKey)
-      ).owner.toBase58()
-    );
-
-    await program.methods
-      .initializeKeypair()
-      .accounts({ keypair: keypair.publicKey })
-      .signers([keypair]) // the signer must be the keypair
+    // transfer 2 SOL
+    const tx2 = await program.methods
+      .donate(new anchor.BN(2_000_000_000))
+      .accounts({
+        pda: pdaAccount,
+      })
       .rpc();
 
     console.log(
-      "owner of keypair after initialize:",
-      (
-        await anchor.getProvider().connection.getAccountInfo(keypair.publicKey)
-      ).owner.toBase58()
+      "lamport balance of pdaAccount",
+      await anchor.getProvider().connection.getBalance(pdaAccount)
     );
 
-    console.log("PDA address:", pda.toBase58());
-    console.log("Keypair address:", keypair.publicKey.toBase58());
-  });
-
-  it("Is initialized!", async () => {
-    const deployer = anchor.web3.Keypair.fromSecretKey(
-      Uint8Array.from(privateKey)
-    );
-
-    const seeds = [];
-    const [myStorage, _bump] = anchor.web3.PublicKey.findProgramAddressSync(
-      seeds,
-      program.programId
-    );
-
-    console.log("the storage account address is", myStorage.toBase58());
-
-    await program.methods.initialize().accounts({ myStorage: myStorage }).rpc();
+    // transfer back 1 SOL
+    // the signer is the permitted address
     await program.methods
-      .changeOwner()
-      .accounts({ myStorage: myStorage })
+      .withdraw(new anchor.BN(1_000_000_000))
+      .accounts({
+        pda: pdaAccount,
+      })
       .rpc();
 
-    // after the ownership has been transferred
-    // the account can still be initialized again
-    await program.methods.initialize().accounts({ myStorage: myStorage }).rpc();
+    console.log(
+      "lamport balance of pdaAccount",
+      await anchor.getProvider().connection.getBalance(pdaAccount)
+    );
   });
 });
