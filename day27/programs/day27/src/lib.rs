15e6a01a8a2ca93823d82645d9ad8807e7b36b00
Nejc Schneider <nejc.sch@gmail.com>
2024-09-14 20:00:28 +0200

    init_if_needed

--- day27/programs/day27/src/lib.rs
+++ day27/programs/day27/src/lib.rs
@@ -1,4 +1,5 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -16,3 +17,22 @@ pub mod day27 {
 
 
 #[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(
+        init_if_needed,
+        payer = signer,
+        space = size_of::<MyPDA>() + 8,
+        seeds = [],
+        bump
+    )]
+    pub my_pda: Account<'info, MyPDA>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MyPDA {
+    pub counter: u64,
+}
