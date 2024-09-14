0cdcace9aa51d9b06c160ce59b2086e03fa7dd42
Nejc Schneider <nejc.sch@gmail.com>
2024-09-14 19:59:29 +0200

    remove initialize()

--- day27/programs/day27/src/lib.rs
+++ day27/programs/day27/src/lib.rs
@@ -6,10 +6,10 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod day27 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
 }
 
+
+
 #[derive(Accounts)]
-pub struct Initialize {}
