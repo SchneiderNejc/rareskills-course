a530e94a9075d007958fbbf62c1f04d6b0368182
Nejc Schneider <nejc.sch@gmail.com>
2024-09-15 16:23:02 +0200

    add set()

--- day28/programs/day28/src/lib.rs
+++ day28/programs/day28/src/lib.rs
@@ -1,6 +1,8 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+declare_id!("8YkLsEFk5ApK9NxdtrXZCVW7tEykAT4aU857ou2U4FNw");
 
 #[program]
 pub mod day28 {
@@ -9,7 +11,33 @@ pub mod day28 {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn set(ctx: Context<Set>, new_val: u32) -> Result<()> {
+        ctx.accounts.pda.value = new_val;
+        Ok(())
+    }
+}
+
+
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = size_of::<PDA>() + 8, seeds = [], bump)]
+    pub pda: Account<'info, PDA>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Set<'info> {
+    #[account(mut)]
+    pub pda: Account<'info, PDA>,
+}
+
+#[account]
+pub struct PDA {
+    pub value: u32,
+}
