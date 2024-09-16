518cb8b0d2a3e3ed78fe052da86a2349f2fd32dc
Nejc Schneider <nejc.sch@gmail.com>
2024-09-16 20:51:28 +0200

    init an account

--- day29/programs/day29/src/lib.rs
+++ day29/programs/day29/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("ohh9qHi1dms4GJCRCUamqPNnHXTGhcmoWAmG41UwJ76");
 
 #[program]
 pub mod day29 {
@@ -12,4 +13,14 @@ pub mod day29 {
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8, seeds = [], bump)]
+    pub my_storage: Account<'info, MyStorage>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MyStorage {}
