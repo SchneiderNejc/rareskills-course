08eaec49b1f1b453428a88afec6cce6833aaaba1
Nejc Schneider <nejc.sch@gmail.com>
2024-09-17 13:43:45 +0200

    tests for account type: UncheckedAccount

--- day31/tests/day31.ts
+++ day31/tests/day31.ts
@@ -5,38 +5,30 @@ import { Day31 } from "../target/types/day31";
 describe("day31", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.Day31 as Program<Day31>;
+  const wallet = anchor.workspace.Day31.provider.wallet;
 
-  // ----------------- Helper functions -----------------
-  async function airdropSol(publicKey, amount) {
-    let airdropTx = await anchor
-      .getProvider()
-      .connection.requestAirdrop(
-        publicKey,
-        amount * anchor.web3.LAMPORTS_PER_SOL
-      );
-
-    await confirmTransaction(airdropTx);
-  }
-
-  async function confirmTransaction(tx) {
-    const latestBlockHash = await anchor
-      .getProvider()
-      .connection.getLatestBlockhash();
-
-    await anchor.getProvider().connection.confirmTransaction({
-      blockhash: latestBlockHash.blockhash,
-      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: tx,
-    });
-  }
-
-  // ----------------- Test functions -----------------
-
-  // @notice Expects to fail. Account is not owned by program
-  it("Wrong owner with Account", async () => {
+  it("Load account with accountInfo", async () => {
+    // CREATE AN ACCOUNT NOT OWNED BY THE PROGRAM
     const newKeypair = anchor.web3.Keypair.generate();
-    await airdropSol(newKeypair.publicKey, 10);
+    const tx = new anchor.web3.Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: newKeypair.publicKey,
+        space: 16,
+        lamports: await anchor
+          .getProvider()
+          .connection.getMinimumBalanceForRentExemption(32),
+        programId: program.programId,
+      })
+    );
 
+    await anchor.web3.sendAndConfirmTransaction(
+      anchor.getProvider().connection,
+      tx,
+      [wallet.payer, newKeypair]
+    );
+
+    // READ THE DATA IN THE ACCOUNT
     await program.methods
       .foo()
       .accounts({ someAccount: newKeypair.publicKey })
