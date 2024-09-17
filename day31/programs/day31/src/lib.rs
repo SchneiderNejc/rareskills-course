ed19802b6faef0d2bacfc67964982b722eace110
Nejc Schneider <nejc.sch@gmail.com>
2024-09-17 12:26:42 +0200

    account type: Account

--- day31/programs/day31/src/lib.rs
+++ day31/programs/day31/src/lib.rs
@@ -1,15 +1,21 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DZAhARBh8xs14dEYi6mQ6S79LPYGEghgqvAbCvvCgVsV");
 
 #[program]
 pub mod day31 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+	pub fn foo(ctx: Context<Foo>) -> Result<()> {
+		// we don't do anything with the account SomeAccount
+		Ok(())
+		}
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Foo<'info> {
+	some_account: Account<'info, SomeAccount>, // When using Account type, must have callable init.
+}
+
+#[account]
+pub struct SomeAccount {}
