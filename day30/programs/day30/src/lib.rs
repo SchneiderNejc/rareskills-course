b8f7d49fbf4a772ba300836113ca004a892275ca
Nejc Schneider <nejc.sch@gmail.com>
2024-09-17 09:37:53 +0200

    delete an account

--- day30/programs/day30/src/lib.rs
+++ day30/programs/day30/src/lib.rs
@@ -1,6 +1,7 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("3wjwbbdFwDsB5HZTqqnJqxhypSq2ytjsBR47XSDVr6PN");
 
 #[program]
 pub mod day30 {
@@ -9,7 +10,34 @@ pub mod day30 {
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
         Ok(())
     }
+
+    pub fn delete(ctx: Context<Delete>) -> Result<()> {
+        Ok(())
+    }
+}
+
+
+
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = size_of::<ThePda>() + 8, seeds = [], bump)]
+    pub the_pda: Account<'info, ThePda>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Delete<'info> {
+    #[account(mut, close = signer, )]
+    pub the_pda: Account<'info, ThePda>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[account]
+pub struct ThePda {
+    pub x: u32,
+}
