5cd599eeaf2d62b1b3d7c4cc2944397e9772453f
Nejc Schneider <nejc.sch@gmail.com>
2024-09-17 13:51:21 +0200

    tests for account type: Signer

--- day31/tests/day31.ts
+++ day31/tests/day31.ts
@@ -5,33 +5,8 @@ import { Day31 } from "../target/types/day31";
 describe("day31", () => {
   anchor.setProvider(anchor.AnchorProvider.env());
   const program = anchor.workspace.Day31 as Program<Day31>;
-  const wallet = anchor.workspace.Day31.provider.wallet;
 
-  it("Load account with accountInfo", async () => {
-    // CREATE AN ACCOUNT NOT OWNED BY THE PROGRAM
-    const newKeypair = anchor.web3.Keypair.generate();
-    const tx = new anchor.web3.Transaction().add(
-      anchor.web3.SystemProgram.createAccount({
-        fromPubkey: wallet.publicKey,
-        newAccountPubkey: newKeypair.publicKey,
-        space: 16,
-        lamports: await anchor
-          .getProvider()
-          .connection.getMinimumBalanceForRentExemption(32),
-        programId: program.programId,
-      })
-    );
-
-    await anchor.web3.sendAndConfirmTransaction(
-      anchor.getProvider().connection,
-      tx,
-      [wallet.payer, newKeypair]
-    );
-
-    // READ THE DATA IN THE ACCOUNT
-    await program.methods
-      .foo()
-      .accounts({ someAccount: newKeypair.publicKey })
-      .rpc();
+  it("Wrong owner with Account", async () => {
+    await program.methods.hello().rpc();
   });
 });
