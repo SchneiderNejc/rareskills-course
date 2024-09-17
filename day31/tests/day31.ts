a961a2ef66bd833cecff11826456b38fab15862d
Nejc Schneider <nejc.sch@gmail.com>
2024-09-17 12:27:04 +0200

    tests for account type: Account

--- day31/tests/day31.ts
+++ day31/tests/day31.ts
@@ -3,14 +3,43 @@ import { Program } from "@coral-xyz/anchor";
 import { Day31 } from "../target/types/day31";
 
 describe("day31", () => {
-  // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.Day31 as Program<Day31>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  // ----------------- Helper functions -----------------
+  async function airdropSol(publicKey, amount) {
+    let airdropTx = await anchor
+      .getProvider()
+      .connection.requestAirdrop(
+        publicKey,
+        amount * anchor.web3.LAMPORTS_PER_SOL
+      );
+
+    await confirmTransaction(airdropTx);
+  }
+
+  async function confirmTransaction(tx) {
+    const latestBlockHash = await anchor
+      .getProvider()
+      .connection.getLatestBlockhash();
+
+    await anchor.getProvider().connection.confirmTransaction({
+      blockhash: latestBlockHash.blockhash,
+      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+      signature: tx,
+    });
+  }
+
+  // ----------------- Test functions -----------------
+
+  // @notice Expects to fail. Account is not owned by program
+  it("Wrong owner with Account", async () => {
+    const newKeypair = anchor.web3.Keypair.generate();
+    await airdropSol(newKeypair.publicKey, 10);
+
+    await program.methods
+      .foo()
+      .accounts({ someAccount: newKeypair.publicKey })
+      .rpc();
   });
 });
