61f6db9fcfc965cfb984bd3fb8e0b81bcae7d835
Nejc Schneider <nejc.sch@gmail.com>
2024-09-18 12:55:41 +0200

    shorthand syntax: make a CPI call to Bob in “one line"

--- day33/programs/alice/src/lib.rs
+++ day33/programs/alice/src/lib.rs
@@ -16,22 +16,29 @@ pub mod alice {
     use super::*;
 
     pub fn ask_day33_to_add(ctx: Context<AliceOp>, a: u64, b: u64) -> Result<()> {
-        let cpi_ctx = CpiContext::new(
-            ctx.accounts.day33_program.to_account_info(),
-            BobAddOp {
-                bob_data_account: ctx.accounts.bob_data_account.to_account_info(),
-            }
-        );
+        // Calls the `bob_add_operation` function in bob program
+        let res = day33::cpi::add_and_store(ctx.accounts.add_function_ctx(), a, b);
 
-        // Correctly reference the CPI function
-        let res = day33::cpi::add_and_store(cpi_ctx, a, b);
-
-        // return an error if the CPI failed
         if res.is_ok() {
             return Ok(());
         } else {
             return err!(Errors::CPIToBobFailed);
         }
+    }
+}
+
+impl<'info> AliceOp<'info> {
+    pub fn add_function_ctx(&self) -> CpiContext<'_, '_, '_, 'info, BobAddOp<'info>> {
+        // The bob program we are interacting with
+        let cpi_program = self.day33_program.to_account_info();
+
+        // Passing the necessary account(s) to the `BobAddOp` account struct in Bob program
+        let cpi_account = BobAddOp {
+            bob_data_account: self.bob_data_account.to_account_info(),
+        };
+
+        // Creates a `CpiContext` object using the new method
+        CpiContext::new(cpi_program, cpi_account)
     }
 }
 
