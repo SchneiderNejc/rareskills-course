ce59d02bb294157ad92a7c5fe5124b355274f173
Nejc Schneider <nejc.sch@gmail.com>
2024-09-18 09:02:21 +0200

    read account balance

--- day32/programs/day32/src/lib.rs
+++ day32/programs/day32/src/lib.rs
@@ -1,15 +1,21 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("ApPuKv6ScvTEEcfKxUN6XXnsifgwXPG9Y9UsXnnhk2c1");
 
 #[program]
 pub mod day32 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn read_balance(ctx: Context<ReadBalance>) -> Result<()> {
+        let balance = ctx.accounts.acct.to_account_info().lamports();
+
+        msg!("balance in Lamports is {}", balance);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct ReadBalance<'info> {
+    /// CHECK: although we read this account's balance, we don't do anything with the information
+    pub acct: UncheckedAccount<'info>,
+}
