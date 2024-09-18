416c204f2781a9a7da342b5bacf916990ef341f9
Nejc Schneider <nejc.sch@gmail.com>
2024-09-18 10:07:03 +0200

    init storage account

--- day33/programs/day33/src/lib.rs
+++ day33/programs/day33/src/lib.rs
@@ -1,15 +1,33 @@
 use anchor_lang::prelude::*;
+use std::mem::size_of;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+declare_id!("DTwEUK34nyL1yQ7DJRd7rFEPwYGfqQ8ph7mRh762uFMY");
 
 #[program]
 pub mod day33 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Data Account Initialized: {}", ctx.accounts.bob_data_account.key());
+
+        Ok(())
+    }
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = size_of::<BobData>() + 8)]
+    pub bob_data_account: Account<'info, BobData>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+#[account]
+pub struct BobData {
+    pub result: u64,
+}
