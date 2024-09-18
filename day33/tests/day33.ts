19ec5bae4e6b45e9945e669f94718b9e76140496
Nejc Schneider <nejc.sch@gmail.com>
2024-09-18 12:10:18 +0200

    test "Is initialized!"

--- day33/tests/day33.ts
+++ day33/tests/day33.ts
@@ -1,16 +1,27 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Day33 } from "../target/types/day33";
+import { Alice } from "../target/types/alice";
 
-describe("day33", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+describe("CPI from Alice to Day33", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Day33 as Program<Day33>;
+  const aliceProgram = anchor.workspace.Alice as Program<Alice>;
+  const dataAccountKeypair = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const tx = await program.methods
+      .initialize()
+      .accounts({
+        bobDataAccount: dataAccountKeypair.publicKey,
+        signer: provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([dataAccountKeypair])
+      .rpc();
+  });
   });
 });
